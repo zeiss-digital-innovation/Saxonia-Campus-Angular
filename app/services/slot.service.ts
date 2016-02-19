c6609071ca28c3ca505510a939fb1dea63c9861c
Sven Hoffmann <sven.hoffmann@saxsys.de>
2016-02-19 17:48:26 +0100

    corrected slots mapping

--- app/services/slot.service.ts
+++ app/services/slot.service.ts
@@ -15,7 +15,7 @@ export class SlotService {
         return this.http.get(this._slotsUrl, {
                 headers: RestService.getHeaders()
             })
-            .map(res => <Slot[]> res.json().data)
+            .map(res => <Slot[]> res.json()._embedded.slots)
             .do(data => console.log(data)) // eyeball results in the console
             .catch(SlotService.handleError)
     }
