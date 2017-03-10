ea369121a0f6249cf657446bc75400211bf1338d
Stefan Bley <stefan.bley@saxsys.de>
2017-03-10 18:09:33 +0100

    Add missing rxjs operators and statics to rxjs-operators.js

--- src/app/rxjs-operators.ts
+++ src/app/rxjs-operators.ts
@@ -1,12 +1,15 @@
 // Statics
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/observable/range';
 import 'rxjs/add/observable/defer';
 
 // Operators
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/groupBy';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/publishReplay';
