fed503f6bf6d9b82eff6d330d5ba046051f82f3a
devwurm <devwurm@devwurm.net>
2017-05-15 10:37:39 +0200

    Minifying assets to reduce bundle size

--- webpack/webpack.prod.partial.js
+++ webpack/webpack.prod.partial.js
@@ -1,9 +1,42 @@
 const { WebpackWarPlugin } = require('webpack-war-plugin');
+const { EnvironmentPlugin, HashedModuleIdsPlugin } = require('webpack');
+const { SuppressExtractedTextChunksWebpackPlugin } = require('@angular/cli/plugins/webpack');
+const { UglifyJsPlugin } = require('webpack').optimize;
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  devtool: false,
+  output: {
+    "path": path.join(process.cwd(), "dist"),
+    "filename": "[name].[chunkhash:20].bundle.js",
+    "chunkFilename": "[id].[chunkhash:20].chunk.js"
+  },
   plugins: [
     new WebpackWarPlugin({
       webInf: 'WEB-INF'
-    })
+    }),
+    new ExtractTextPlugin({
+      "filename": "[name].[contenthash:20].bundle.css"
+    }),
+    new SuppressExtractedTextChunksWebpackPlugin(),
+    new EnvironmentPlugin({
+      "NODE_ENV": "production"
+    }),
+    new HashedModuleIdsPlugin({
+      "hashFunction": "md5",
+      "hashDigest": "base64",
+      "hashDigestLength": 4
+    }),
+    new UglifyJsPlugin({
+      "mangle": {
+        "screw_ie8": true
+      },
+      "compress": {
+        "screw_ie8": true,
+        "warnings": false
+      },
+      "sourceMap": false
+    }),
   ]
 };
