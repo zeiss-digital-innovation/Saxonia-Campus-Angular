d91b89d4c8a973734a287f0ceca483e685dc6695
Stefan Bley <stefan.bley@saxsys.de>
2018-04-06 14:58:32 +0200

    Add configuration for nb550

--- src/environments/environment.nb550.ts
+++ src/environments/environment.nb550.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: false,
 
   'adfs.auth.url': 'https://adfs.saxsys.de/adfs/oauth2/authorize',
-  'adfs.token.url': 'https://nb323.saxsys.de:8443/rest/api/tokenHandler',
+  'adfs.token.url': 'https://nb550.saxsys.de:8443/rest/api/tokenHandler',
   'adfs.logout.url': 'https://adfs.saxsys.de/adfs/ls/?wa=wsignout1.0',
   'client.id': 'campusapp',
-  'resource': 'https://nb323.saxsys.de:8443/adfs-saml',
-  'redirect.url': 'https://nb323.saxsys.de:4200/campus',
-  'backend.url': 'https://nb323.saxsys.de:8443/rest/api',
+  'resource': 'https://nb550.saxsys.de:8443/adfs-saml',
+  'redirect.url': 'https://nb550.saxsys.de:4200/campus',
+  'backend.url': 'https://nb550.saxsys.de:8443/rest/api',
   'app.mode': 'overview'
 };
