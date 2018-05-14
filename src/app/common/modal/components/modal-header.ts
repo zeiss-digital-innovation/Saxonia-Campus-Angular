e884449d38629437641d61227a2644fb37ee8f9f
Stefan Bley <stefan.bley@saxsys.de>
2018-05-14 12:51:46 +0200

    Fix modal header

--- src/app/common/modal/components/modal-header.ts
+++ src/app/common/modal/components/modal-header.ts
@@ -1,17 +1,13 @@
-import {Component, Input} from '@angular/core';
-import {ModalComponent} from './modal';
+import { Component, Input } from '@angular/core';
+import { ModalComponent } from './modal';
 
 @Component({
-    selector: 'modal-header',
-    template: `
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close" (click)="modal.dismiss()" 
-              [hidden]="!showClose"><span aria-hidden="true">&#215;</span></button>
-            <ng-content></ng-content>
-        </div>
-    `
+  selector: 'modal-header',
+  templateUrl: './modal-header.html'
 })
 export class ModalHeaderComponent {
-    @Input('show-close') showClose = false;
-    constructor(public modal: ModalComponent) { }
+  @Input('show-close') showClose = false;
+
+  constructor(public modal: ModalComponent) {
+  }
 }
